625d37be4472d9b28dabd5aeffe3842871cbc3d3
suaibehaque <69385704+suaibehaque@users.noreply.github.com>
2021-04-26 17:49:30 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,169 @@
+// tanvir
+function pulsTan(){
+	
+	document.getElementById("puls_tan").style.borderColor="#ffff00";
+	
+ setTimeout(function puls(){
+	 
+	document.getElementById("puls_tan").style.borderColor="#dcdcdc ";
+	 
+ },450);
+	
+}
+
+// hiya
+function pulsHiya(){
+	
+	document.getElementById("puls_hiya").style.borderColor="#ffff00";
+	
+ setTimeout(function puls(){
+	 
+	document.getElementById("puls_hiya").style.borderColor="#dcdcdc ";
+	 
+ },550);
+	
+}
+// mim
+function pulsMim(){
+	
+	document.getElementById("puls_mim").style.borderColor="#ffff00";
+	
+ setTimeout(function puls(){
+	 
+	document.getElementById("puls_mim").style.borderColor="#dcdcdc ";
+	 
+ },550);
+	
+}
+// prottoy
+function pulsProttoy(){
+	
+	document.getElementById("puls_protoy").style.borderColor="#ffff00";
+	
+ setTimeout(function puls(){
+	 
+	document.getElementById("puls_protoy").style.borderColor="#dcdcdc ";
+	 
+ },550);
+	
+}
+// somrat
+function pulsSomrat(){
+	
+	document.getElementById("puls_somrat").style.borderColor="#ffff00";
+	
+ setTimeout(function puls(){
+	 
+	document.getElementById("puls_somrat").style.borderColor="#dcdcdc ";
+	 
+ },550);
+	
+}
+// jamal
+function pulsJamal(){
+	
+	document.getElementById("puls_jamal").style.borderColor="#ffff00";
+	
+ setTimeout(function puls(){
+	 
+	document.getElementById("puls_jamal").style.borderColor="#dcdcdc ";
+	 
+ },550);
+	
+}
+// tomal
+function pulsTomal(){
+	
+	document.getElementById("puls_tomal").style.borderColor="#ffff00";
+	
+ setTimeout(function puls(){
+	 
+	document.getElementById("puls_tomal").style.borderColor="#dcdcdc ";
+	 
+ },550);
+	
+}
+// fairuz
+function pulsFairuz(){
+	
+	document.getElementById("puls_fairuz").style.borderColor="#ffff00";
+	
+ setTimeout(function puls(){
+	 
+	document.getElementById("puls_fairuz").style.borderColor="#dcdcdc ";
+	 
+ },550);
+	
+}
+// riad
+function pulsRiad(){
+	
+	document.getElementById("puls_riad").style.borderColor="#ffff00";
+	
+ setTimeout(function puls(){
+	 
+	document.getElementById("puls_riad").style.borderColor="#dcdcdc ";
+	 
+ },550);
+	
+}
+// sourov
+function pulsSowrov(){
+	
+	document.getElementById("puls_sourov").style.borderColor="#ffff00";
+	
+ setTimeout(function puls(){
+	 
+	document.getElementById("puls_sourov").style.borderColor="#dcdcdc ";
+	 
+ },550);
+	
+}
+// takber
+function pulsTakber(){
+	
+	document.getElementById("puls_takber").style.borderColor="#ffff00";
+	
+ setTimeout(function puls(){
+	 
+	document.getElementById("puls_takber").style.borderColor="#dcdcdc ";
+	 
+ },550);
+	
+}
+// noman
+function pulsNoman(){
+	
+	document.getElementById("puls_noman").style.borderColor="#ffff00";
+	
+ setTimeout(function puls(){
+	 
+	document.getElementById("puls_noman").style.borderColor="#dcdcdc ";
+	 
+ },550);
+	
+}
+// emon
+function pulsEmon(){
+	
+	document.getElementById("puls_emon").style.borderColor="#ffff00";
+	
+ setTimeout(function puls(){
+	 
+	document.getElementById("puls_emon").style.borderColor="#dcdcdc ";
+	 
+ },550);
+	
+}
+// suaibe
+function pulsSuaibe(){
+	
+	document.getElementById("puls_suaibe").style.borderColor="#ffff00";
+	
+ setTimeout(function puls(){
+	 
+	document.getElementById("puls_suaibe").style.borderColor="#dcdcdc ";
+	 
+ },550);
+	
+}
